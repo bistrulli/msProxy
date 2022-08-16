f0be6ea7f933d974dabe075afa2e9795ce24e685
emilio <emilio@mpbwalt.local>
2022-08-16 17:50:19 +0200

    minor

--- src/main/java/proxyLogic/AcquireHandler.java
+++ src/main/java/proxyLogic/AcquireHandler.java
@@ -14,28 +14,27 @@ import app.Proxy;
 public class AcquireHandler implements HttpHandler {
 
 	private Proxy prx = null;
-	private List<Integer> ports=null;
-	
+	private List<Integer> ports = null;
+
 	public AcquireHandler(Proxy prx) {
 		this.prx = prx;
-		this.ports=null;
+		this.ports = null;
 	}
 
 	public Integer pickReplica() {
 		// per il momento e raound robin
-		if(this.ports==null)
+		if (this.ports == null)
 			this.ports = ((List<Integer>) this.prx.getMs().get("ports"));
-			
-		int port=this.ports.get(0);
+
+		int port = this.ports.get(0);
 		Collections.rotate(this.ports, 1);
 		return port;
 	}
 
 	public void forwardRqt(Integer port, HttpExchange req) {
-		// qui la logica per l'effettivo foward della richiesta
-		Constructor<? extends Runnable> c = null;
 		try {
-			c = this.prx.getPrxLogic().getDeclaredConstructor(Integer.class, HttpExchange.class, Proxy.class);
+			Constructor<? extends Runnable> c = this.prx.getPrxLogic().getDeclaredConstructor(Integer.class,
+					HttpExchange.class, Proxy.class);
 			this.prx.getThreadpool().submit(c.newInstance(port, req, this.prx));
 		} catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException
 				| IllegalArgumentException | InvocationTargetException e) {

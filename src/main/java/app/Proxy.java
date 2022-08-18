e75a6fee9c92ec035c488b55db547f0f3a43bc8e
emilio <emilio@mpbwalt.local>
2022-08-18 09:37:58 +0200

    minor

--- src/main/java/app/Proxy.java
+++ src/main/java/app/Proxy.java
@@ -33,7 +33,8 @@ public class Proxy {
 		try {
 			this.server = HttpServer.create(new InetSocketAddress("localhost", port), this.backlogSize);
 			this.server.createContext("/", new AcquireHandler(this));
-			this.server.setExecutor(java.util.concurrent.Executors.newCachedThreadPool());
+			this.server.setExecutor((ThreadPoolExecutor) Executors.newFixedThreadPool(20));
+			//this.server.setExecutor(java.util.concurrent.Executors.newCachedThreadPool());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
